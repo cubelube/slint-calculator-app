5846b24275d98beebebcaccb0469392ab87f3fe2
cubelube <114615759+cubelube@users.noreply.github.com>
2024-01-18 20:45:00 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,62 @@
 use slint::SharedString;
+use regex::Regex;
+use lazy_static::lazy_static;
+
 slint::include_modules!();
+
+lazy_static! {
+    static ref FIND_SYMBOL: Regex = Regex::new(r"(\+|-|\*|\/)").unwrap();
+}
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
-    ui.on_add_to_text_area(move |string1, string2|{
+    ui.on_add_to_text_area(move |existing_string, new_string|{
 
         let ui = ui_handle.unwrap();
-        match string2.to_string().as_str() {
+        match new_string.to_string().as_str() {
             "C" => {let result = format!(""); ui.set_textarea(result.into());},
-            "=" => {let result = format!("{}", get_calculation(&string1).to_string()); ui.set_textarea(result.into());},
-            _ => {let result = format!("{}{}", string1, string2); ui.set_textarea(result.into());}
+            "=" => {let result = format!("{}", get_calculation(&existing_string).to_string()); ui.set_textarea(result.into());},
+            _ => {let result = format!("{}{}", existing_string, new_string); ui.set_textarea(result.into());}
         }
     });
 
     ui.run()
 }
 
-fn get_calculation(input: &SharedString) -> f64 {
+fn get_calculation(input: &SharedString) -> String {
     let symbols = vec!["+", "-", "*", "/"];
 
-    for symbol in symbols {
-        match input.as_str().find(symbol) {
-            Some(_) => {
-                let n1: f64 = input.split(symbol).nth(0).expect("FAILED").parse().expect("FAILED");
-                let n2: f64 = input.split(symbol).nth(1).expect("FAILED").parse().expect("FAILED");
-
-                match symbol {
-                    "+" => return n1 + n2,
-                    "-" => return n1 - n2,
-                    "*" => return n1 * n2,
-                    "/" => return n1 / n2,
-                    _ => return 1.0
-                };
-            }
-
-            _ => continue
-        };
+    if FIND_SYMBOL.is_match(input) {
+        for symbol in symbols {
+            match input.as_str().find(symbol) {
+                Some(_) => {
+                    let n1: f64 = convert(input.to_string().split(symbol).nth(0).expect("FAILED"));
+                    let n2: f64 = convert(input.to_string().split(symbol).nth(1).expect("FAILED"));
+    
+                    match symbol {
+                        "+" => return (n1 + n2).to_string(),
+                        "-" => return (n1 - n2).to_string(),
+                        "*" => return (n1 * n2).to_string(),
+                        "/" => return (n1 / n2).to_string(),
+                        _ => return String::from("Failed (for some reason)")
+                    };
+                }
+    
+                _ => continue
+            };
+        }
+    } else {
+        return convert(input).to_string();
     }
 
-    return 1.0;
+    return String::from("Failed (for some reason)")
+}
+
+fn convert(input: &str) -> f64 {
+    match input.find("%") {
+        Some(_) => {let output: f64 = input.split("%").nth(0).expect("FAILED").parse().expect("FAILED"); return output / 100.0},
+        _ => {let output: f64 = input.parse().expect("FAILED"); return output}
+    };
 }
